1fa20af9b2f94c996475c226bad7694c9f70ec43
Sakib137 <mdsakib124202@gmail.com>
2024-12-29 23:05:55 +0600

    splash done

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/apps.dart';
+
+void main() {
+  runApp(const TaskManagerApp());
+}
---
+++ lib/ui/utills/assets_path.dart
@@ -0,0 +1,6 @@
+class AssetsPath {
+  static const String _assetsPath = 'assets/images';
+
+  static const String logoSvg = '$_assetsPath/logo.svg';
+  static const String backgroundSvg = '$_assetsPath/background.svg';
+}
