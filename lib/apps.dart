74971520db95302d75bbf2b045bd381fc323ddd2
Sakib137 <mdsakib124202@gmail.com>
2024-12-30 22:47:36 +0600

    sign in complete

--- lib/apps.dart
+++ lib/apps.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screen/splash_screen.dart';
 import 'package:task_manager/ui/screen/sign_in_screen.dart';
+import 'package:task_manager/ui/utills/app_colour.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
@@ -9,6 +10,37 @@ class TaskManagerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w100,
+          ),
+          contentPadding: EdgeInsets.all(16),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColour.themeColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            fixedSize: const Size.fromWidth(double.maxFinite),
+            padding: const EdgeInsets.all(8),
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
       onGenerateRoute: (RouteSettings settings) {
         late Widget widget;
         if (settings.name == SplashScreen.name) {
--- lib/ui/screen/sign_in_screen.dart
+++ lib/ui/screen/sign_in_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/utills/app_colour.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -12,9 +15,77 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("hi"),
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Text("Get Started With", style: textTheme.titleLarge),
+                const SizedBox(
+                  height: 24,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_forward_rounded),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text("Forgot Password?"),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w100),
+                            children: [
+                              TextSpan(
+                                  text: "Sign Up",
+                                  style: const TextStyle(
+                                    color: AppColour.themeColor,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {}),
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
